7b9956c0206aea47e5d7b296c7f8360fd685c4d7
amilbek <73268271+amilbek@users.noreply.github.com>
2021-03-18 22:36:01 +0600

    Update IClientRepository.java

--- src/com/company/repositories/interfaces/IClientRepository.java
+++ src/com/company/repositories/interfaces/IClientRepository.java
@@ -4,6 +4,7 @@ import com.company.entities.Clients;
 
 public interface IClientRepository {
     Clients clientSignIn(String login, String password);
+    Clients clientCheck(String login);
 
     boolean addClient(Clients client);
 
